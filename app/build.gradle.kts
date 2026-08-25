14855e327b70c7f5684dc2ec45a8a08ec313e64a
hajs0208 <156035391+hajs110208@users.noreply.github.com>
2026-08-25 20:28:27 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
     // Both environments grouped together cleanly inside ONE block
     buildTypes {
         debug {
-            signingConfig = signingConfigs.getByName("stableDebug")
+            signingConfig null
         }
         release {
             isMinifyEnabled = false
